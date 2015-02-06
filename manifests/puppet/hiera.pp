5845afe542ba3b515ea71b996f298f1e5e23684b
Clayton Burlison <clburlison@gmail.com>
2015-02-06 15:02:47 -0600

    remove hiera data this is stored in a separate repo

--- manifests/puppet/hiera.pp
+++
@@ -1,9 +0,0 @@
-class profile::puppet::hiera {
-  file {'/etc/puppet/hiera.yaml':
-    ensure => present,
-    source => 'puppet:///modules/profile/puppet/hiera.yaml',
-    group  => 'staff',
-    owner  => 'puppet',
-    mode   => '755',
-  }
-}
