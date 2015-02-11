c2fc5250193a9e6081fd9a1776964ff7c7ac386c
Clayton Burlison <clburlison@gmail.com>
2015-02-11 13:40:31 -0600

    need double quotes for var to be parsed

--- manifests/profiles/base_profiles.pp
+++ manifests/profiles/base_profiles.pp
@@ -9,21 +9,21 @@ class profile::profiles::base_profiles (
     # the name needs to be the exact same as the PayloadIdentifier (case senitive)
     config_profile { 'BISD-GeekTool':
       ensure    => present,
-      path      => '${puppet_path}/BISD-GeekTool.mobileconfig',
+      path      => "${puppet_path}/BISD-GeekTool.mobileconfig",
       system    => true,
     }
 
     # requires that GeekTools is present. Maybe make a fact to look for GeekTool app...if it exsits then run config.    
     # config_profile { 'Geektool-login-item':
     #   ensure    => present,
-    #   path      => '${puppet_path}/Geektool-login-item.mobileconfig',
+    #   path      => "${puppet_path}/Geektool-login-item.mobileconfig",
     #   system    => true,
     # }
     
     if $mac_laptop == mac_laptop {
       config_profile { 'BISD-GeekTool':
         ensure    => present,
-        path      => '${puppet_path}/BISD-Secure_System.mobileconfig',
+        path      => "${puppet_path}/BISD-Secure_System.mobileconfig",
         system    => true,
       }
     }
