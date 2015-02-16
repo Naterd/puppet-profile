ff8c0c64fb9b14ad8c462c3435212f8f0409434f
Clayton Burlison <clburlison@gmail.com>
2015-02-16 17:36:24 -0600

    vmware tools needs to install if type is vmware

--- manifests/mac_apps/vmware_tools.pp
+++ manifests/mac_apps/vmware_tools.pp
@@ -1,7 +1,7 @@
 # Install VMware Tools from the internet
 class profile::mac_apps::vmware_tools {
   
-  if $virtual == 'virtual' {
+  if $virtual == 'vmware' {
     package { 'vmwaretools':
       source   => "https://www.dropbox.com/s/wartjc2zbbyb5gr/VMwareTools.pkg.zip?raw=1",
       provider => 'compressed_pkg',
