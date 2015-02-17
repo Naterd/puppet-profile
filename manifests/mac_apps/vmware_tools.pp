300e38d4757590285207ac979e986312b7b71c0f
Clayton Burlison <clburlison@gmail.com>
2015-02-16 20:34:39 -0600

    vmware tools as dmg instead of compressedPkg

--- manifests/mac_apps/vmware_tools.pp
+++ manifests/mac_apps/vmware_tools.pp
@@ -3,8 +3,8 @@ class profile::mac_apps::vmware_tools {
   
   if $virtual == 'vmware' {
     package { 'vmwaretools':
-      source   => "https://www.dropbox.com/s/wartjc2zbbyb5gr/VMwareTools.pkg.zip?raw=1",
-      provider => 'compressed_pkg',
+      source   => "https://www.dropbox.com/s/64z7v8nzbhaw31a/VMwareTools.dmg?raw=1",
+      provider => 'pkgdmg',
     }
   }
 }
