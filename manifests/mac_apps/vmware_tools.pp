df316139f18a9a8f7e6c3453bc31c0d048a79124
Clayton Burlison <clburlison@gmail.com>
2015-02-16 20:41:45 -0600

    new vmware tools URL.
    Provider didn't like Dropbox URL.

--- manifests/mac_apps/vmware_tools.pp
+++ manifests/mac_apps/vmware_tools.pp
@@ -3,7 +3,7 @@ class profile::mac_apps::vmware_tools {
   
   if $virtual == 'vmware' {
     package { 'vmwaretools':
-      source   => "https://www.dropbox.com/s/64z7v8nzbhaw31a/VMwareTools.dmg?raw=1",
+      source   => "http://cburlison.s3.amazonaws.com/public/VMwareTools.dmg",
       provider => 'pkgdmg',
     }
   }
