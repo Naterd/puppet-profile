e4715390e9c54fb7465b3d4736079cfa59ef9336
Clayton Burlison <clburlison@gmail.com>
2015-05-11 23:01:43 -0500

    only install beamoff on yoyo

--- manifests/mac_apps/beamoff.pp
+++ manifests/mac_apps/beamoff.pp
@@ -1,17 +1,24 @@
 # Install Beamoff from the internet
 class profile::mac_apps::beamoff {
   $Agent = 'puppet:///modules/k12.bisd.launch_beamoff.plist'
+  $min_os_version = '10.10'
 
   if $virtual == 'vmware' {
-    package { 'Beamoff':
-      source   => "http://cburlison.s3.amazonaws.com/public/beamoff.zip",
-      provider => 'compressed_app',
+    if version_compare($::macosx_productversion_major, $min_os_version) < 0 {
+      #notice('This operating system does not need Beamoff.')
     }
-		
-    file {"/Library/LaunchAgents/k12.bisd.launch_beamoff.plist":
-      source  => $Agent,
-      owner   => root,
-      group   => wheel,
-      mode    => '0664',
-  }
+    else {
+      package { 'Beamoff':
+        source   => "http://cburlison.s3.amazonaws.com/public/beamoff.zip",
+        provider => 'compressed_app',
+      }
+    
+      file {"/Library/LaunchAgents/k12.bisd.launch_beamoff.plist":
+        source  => $Agent,
+        owner   => root,
+        group   => wheel,
+        mode    => '0664',
+      }
+    }
+  }    
 }
