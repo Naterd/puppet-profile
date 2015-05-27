a9297be22f2289b4d1d5a37f861c2e3ead86992e
Clayton Burlison <clburlison@gmail.com>
2015-05-27 13:25:14 -0500

    once more dockutil..refactored

--- manifests/mac_settings/dock.pp
+++ manifests/mac_settings/dock.pp
@@ -1,22 +1,29 @@
 class profile::mac_settings::dock {
-  $script = hiera('outsetdock::script', 'puppet:///modules/profile/dockutil/district_dock.sh')
-  $freq   = hiera('outsetdock::freq', 'once') 
+  $script      = hiera('outsetdock::script', 'puppet:///modules/profile/dockutil/district_dock.sh')
+  $freq        = hiera('outsetdock::freq', 'once') 
+  $ensure      = hiera('outsetdock::ensure', 'present')
+  $script_name = hiera('outsetdock::script', '5-dock.sh')
+  $outset_path = '/usr/local/outset/login-'
 
-  if $freq == 'once' {
-    file {"/usr/local/outset/login-once/dock.sh":
+  if $ensure == 'present' {
+    file {"${outset_path}${freq}/${script_name}":
+      ensure => absent,
       source  => $script,
       owner   => root,
       group   => wheel,
       mode    => '0755',
+      notify => Exec["remove_dock_once"],
     }
   }
-
-  if $freq == 'every' {
-    file {"/usr/local/outset/login-every/dock.sh":
-      source  => $script,
-      owner   => root,
-      group   => wheel,
-      mode    => '0755',
+  
+  if $ensure == 'absent' {
+    file {"${outset_path}${freq}/${script_name}":
+      ensure => absent,
     }
+  }
+  
+  exec { 'remove_dock_once':
+    command     => "/etc/puppet/environments/production/modules/profile/files/dockutil/remove_dock_once.sh ${script_name}",
+    refreshonly => true,
   }
 }
