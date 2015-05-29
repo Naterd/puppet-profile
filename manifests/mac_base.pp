b1a4655f288f0769227778a752f9c2b7f76a2586
Clayton Burlison <clburlison@gmail.com>
2015-05-29 09:48:26 -0500

    new error msg

--- manifests/mac_base.pp
+++ manifests/mac_base.pp
@@ -1,13 +1,13 @@
 # Base class that gets applied to all Macs
 class profile::mac_base {
-  if $::osfamily != 'Darwin' {
-    fail("unsupported osfamily: ${::osfamily}")
-  }
+  # if $::osfamily != 'Darwin' {
+  #   fail("unsupported osfamily: ${::osfamily}")
+  # }
 
   $min_os_version = '10.9'
 
   if version_compare($::macosx_productversion_major, $min_os_version) < 0 {
-    notice('This is an unsupported operating system version. You are older than 10.9.')
+    notice('Managedmac is unsupported on this operating system version. You are older than 10.9.')
   }
   else {
     include managedmac
