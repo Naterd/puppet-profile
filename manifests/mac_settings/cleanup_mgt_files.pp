cff88ee62a445a3d71f974a6f9554a25846a4110
Clayton Burlison <clburlison@gmail.com>
2015-07-23 16:26:03 -0500

    remove old file

--- manifests/mac_settings/cleanup_mgt_files.pp
+++ manifests/mac_settings/cleanup_mgt_files.pp
@@ -8,4 +8,8 @@ class profile::mac_settings::cleanup_mgt_files {
   file { "/usr/local/outset/login-once/bisd_audiovideo_desktop.sh":
     ensure => absent,
   }
+  
+  file { "/usr/local/outset/login-once/5-dock.sh":
+    ensure => absent,
+  }
 }
