1fd7ca3269292ca6c5570a55d50ad8e92511fb44
Clayton Burlison <clburlison@gmail.com>
2015-07-23 16:28:31 -0500

    install vmware tools from internet
    This might break something...

--- manifests/mac_apps/vmware_tools.pp
+++ manifests/mac_apps/vmware_tools.pp
@@ -1,10 +1,21 @@
-# Install VMware Tools from the internet
+# Install vmware tools from the vmware website
 class profile::mac_apps::vmware_tools {
-  
+  $script = '/etc/puppet/environments/production/modules/profile/files/vmware/install_vmware_tools.sh'
+
   if $virtual == 'vmware' {
-    package { 'vmwaretools':
-      source   => "http://cburlison.s3.amazonaws.com/public/VMwareTools.dmg",
-      provider => 'pkgdmg',
+    
+    file {'/private/tmp/install_vmware_tools.sh':
+        ensure => present,
+        source  => $script,
+        owner   => root,
+        group   => wheel,
+        mode    => '0755',
+    }
+    
+    exec { "Install vmware tools":
+        command => $script,
+        creates => "/Library/Application Support/VMware Tools/vmware-tools-cli",
+        require => File['/private/tmp/install_vmware_tools.sh'],
     }
   }
 }
