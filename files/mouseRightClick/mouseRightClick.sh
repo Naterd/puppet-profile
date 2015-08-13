426d30d24fa5f3fcd3b214f950e9b09b85b6f0b6
Clayton Burlison <clburlison@gmail.com>
2015-08-13 11:21:54 -0500

    don't quote pref path

--- files/mouseRightClick/mouseRightClick.sh
+++ files/mouseRightClick/mouseRightClick.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Enable right click on Mouse. Yosemite doesn't respect profiles for this setting.
 
-/usr/bin/defaults write "~/Library/Preferences/com.apple.driver.AppleHIDMouse" Button2 -int 2
-/usr/bin/defaults write "~/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.mouse" MouseButtonMode -string TwoButton
-/usr/bin/defaults write "~/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad" TrackpadRightClick -int 1
+/usr/bin/defaults write ~/Library/Preferences/com.apple.driver.AppleHIDMouse Button2 -int 2
+/usr/bin/defaults write ~/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string TwoButton
+/usr/bin/defaults write ~/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -int 1
 exit 0
